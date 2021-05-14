2af1ea8897e1a9d22f48d9cf85a5a7c3c96979d2
Tiago Lindgren Curi <tcuri@unicad.local>
2021-05-14 17:48:26 -0300

    primeiro commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+
+
+import Head from 'next/head';
+
+
+export default function Home() {
+  return (
+    [
+      <Head>
+        <title>Início | ignews</title>
+      </Head>,
+      <h1>
+        Hello <span>World</span> 
+      </h1>
+    ]
+  )
+}
