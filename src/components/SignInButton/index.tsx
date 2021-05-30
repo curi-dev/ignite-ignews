fe0ec0998418b7579faf4dee88c597337b6575e7
Tiago Lindgren Curi <shopper.tiago@gmail.com>
2021-05-30 13:50:44 -0300

    retrieving user subscription

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();    
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button className={styles.signInButton}>
             <FaGithub color="#84d361" />
-            Tiago Lindgren Curi
-            <FiX color="#737388" className={styles.closeIcon} />
+            {session.user.name}
+            <FiX 
+             color="#737388" 
+             className={styles.closeIcon} 
+             onClick={() => signOut()}
+            />
         </button>
     ) : (
         <button 
          type="button"
          className={styles.signInButton}
+         onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
